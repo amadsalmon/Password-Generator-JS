1742331b6e56a428337a2c9519f4c6504bdea1c3
Amad Salmon <amad.salmon@icloud.com>
2021-01-11 01:36:20 +0100

    Automatically fill the length selector's options

---
+++ script.js
@@ -0,0 +1,29 @@
+// Automatically fill the length selector's options
+fillLengthOptions();
+
+/* This functions fills the length selector's options with values ranging from 1 to 128. */
+function fillLengthOptions() {
+    var select = document.getElementById("length-selector");
+    var option;
+
+    // Create "Weak" separator
+    var optionGroup = document.createElement('optgroup');
+    optionGroup.setAttribute('label', "Weak");
+    select.appendChild(optionGroup);
+
+    var i = 0;
+    for (i = 1; i <= 128; i++) {
+        option = document.createElement('option');
+        option.setAttribute('value', i);
+        option.appendChild(document.createTextNode(i));
+        if (i == 16) {
+            // Create "Strong" separator
+            optionGroup = document.createElement('optgroup');
+            optionGroup.setAttribute('label', "Strong");
+            select.appendChild(optionGroup);
+            // Make option "16" selected by default.
+            option.setAttribute('selected', "selected");
+        }
+        optionGroup.appendChild(option);
+    }
+}
